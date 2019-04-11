430ec0789d0265b39bd2b5a54f5df705fa998c1f
SergeyKarasyov <sergeyvkarasyov@gmail.com>
2019-04-11 06:23:50 +0200

    fifth chapter

---
+++ ch5/stdthread2/thread_example.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+std::mutex display_mutex;
+
+void worker()
+{
+    std::thread::id this_id = std::this_thread::get_id();
+    display_mutex.lock();
+    std::cout << "thread " << this_id << " sleeping" << std::endl;
+    display_mutex.unlock();
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+int main()
+{
+    std::thread t1(worker);
+    std::thread::id t1_id = t1.get_id();
+
+    std::thread t2(worker);
+    std::thread::id t2_id = t2.get_id();
+
+    display_mutex.lock();
+    std::cout << "t1 -> " << t1_id << std::endl;
+    std::cout << "t2 -> " << t2_id << std::endl;
+    display_mutex.unlock();
+
+
+    t1.join();
+    t2.join();
+    return 0;
+}
