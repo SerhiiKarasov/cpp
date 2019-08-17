b5b14dfcc17c81089f47bce46d10b86d8b8afb1d
SergeyKarasyov <sergeyvkarasyov@gmail.com>
2019-08-17 23:25:03 +0200

    first ui example + style + translation

---
+++ Hello_Qt_OpenCV/Hello_Qt_OpenCV_withTranslation/translation_ua.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Blurrer</source>
+        <translation>Блюрер</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <source>Input Image:</source>
+        <translation>Початкова картинка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <location filename="mainwindow.ui" line="86"/>
+        <source>browse</source>
+        <translation>пошук</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="48"/>
+        <source>Filter type:</source>
+        <translation>Тип фільтру</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>Median Blur</source>
+        <translation>Медіанна фільтрація</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>Gaussian Blur</source>
+        <translation>Гаусівська фільтрація</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <source>Output Image:</source>
+        <translation>Результат</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>Display Image after Saving</source>
+        <translation>Показати результат по збереженню</translation>
+    </message>
+</context>
+</TS>
